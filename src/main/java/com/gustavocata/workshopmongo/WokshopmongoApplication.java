cbbab86fe632320244f86f6f26ff9e06f93c22f1
gustavocata <gustavo.catarino10@hotmail.com>
2021-10-31 17:35:57 -0300

    Projeto Criado

---
+++ src/main/java/com/gustavocata/workshopmongo/WokshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavocata.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WokshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WokshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavocata/workshopmongo/WokshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavocata.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WokshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
