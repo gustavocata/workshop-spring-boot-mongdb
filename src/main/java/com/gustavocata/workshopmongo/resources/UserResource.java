bd11acad92c91668dc5dcc91af52572a5a415045
gustavocata <gustavo.catarino10@hotmail.com>
2021-11-02 01:09:06 -0300

    Inserção de usuário com Post

--- src/main/java/com/gustavocata/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gustavocata/workshopmongo/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.gustavocata.workshopmongo.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gustavocata.workshopmongo.domain.User;
 import com.gustavocata.workshopmongo.dto.UserDTO;
@@ -26,6 +30,19 @@ public class UserResource {
 	public ResponseEntity<List<UserDTO>> findAll(){	
 		List <User> list = service.findAll();
 		List <UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);
+		return ResponseEntity.ok().body(listDto);
+	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/gustavocata/workshopmongo/services/UserService.java
+++ src/main/java/com/gustavocata/workshopmongo/services/UserService.java
@@ -2,20 +2,37 @@ package com.gustavocata.workshopmongo.services;
 
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavocata.workshopmongo.domain.User;
+import com.gustavocata.workshopmongo.dto.UserDTO;
 import com.gustavocata.workshopmongo.repository.UserRepository;
+import com.gustavocata.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
+   @Autowired
+	private UserRepository repo;
 	
-@Autowired
-private UserRepository repo;
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+			
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	}
 
-public List<User> findAll(){
-	return repo.findAll();
-}
-}
+
